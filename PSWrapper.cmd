bb9bde39863ef87d9555b68c27106862bd2e1dfc
Masanori KOMAGAMINE <github@ringo.mm.st>
2016-07-04 23:52:25 +0900

    Update PSWrapper.cmd

--- PSWrapper.cmd
+++ PSWrapper.cmd
@@ -11,9 +11,11 @@
 ######################################## POWERSHELL CODE BLOCK ########################################
 
 <#
-  This is a generic batch file wrapper for a Powershell script.
-  This script is a modified one. For the original source code,
-  refer Dave Amenta's post at his blog [*1]
+  This is a generic batch file wrapper for a Powershell script
+  and enable the PowerShell script to be executed even if the ExecutionPolicy is configured as "Restricted".
+
+  This wrapper is a modified version of Dave Amenta's code.
+  For the original source code, refer his post. [*1]
 
   [*1] "Embed PowerShell inside a batch file"
   http://www.daveamenta.com/2013-03/embed-powershell-inside-a-batch-file/
@@ -28,47 +30,47 @@ Add-Type -AssemblyName System.Windows.Forms
 
 function Main
 {
-	$script:selfPath = [environment]::GetEnvironmentVariable("PS_WRAPPER_PATH")
-	$script:cmdPid = (gwmi win32_process -Filter "processid='$pid'").parentprocessid
+  $script:selfPath = [environment]::GetEnvironmentVariable("PS_WRAPPER_PATH")
+  $script:cmdPid = (gwmi win32_process -Filter "processid='$pid'").parentprocessid
 
-	hideWindow $script:cmdPid   # Prevent user from closing command prompt
+  hideWindow $script:cmdPid   # Prevent user from closing command prompt
 
 ### WRITE YOUR CODE HERE ###
 
-	$message = ""
-	if ($script:args.Count -gt 0) {
-		$message = "Command line parameters (or dropped files) :`n`n" + $script:args
-	} else {
-		$message = "You can specify command line parameters as follows :`n`n> " + $script:selfPath + " This is a test"
-	}
-	msgboxInfo "Generic batch file wrapper for a Powershell script" $message
+  $message = ""
+  if ($script:args.Count -gt 0) {
+    $message = "Command line parameters (or dropped files) :`n`n" + $script:args
+  } else {
+    $message = "You can specify command line parameters as follows :`n`n> " + $script:selfPath + " This is a test"
+  }
+  msgboxInfo "Generic batch file wrapper for a Powershell script" $message
 }
 
 
 function hideWindow
 {
-	param (
-		[Parameter(Mandatory=$true)]
-		[UInt32]
-		$process_id
-	)
-	$windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);' 
-	$asyncwindow = Add-Type -MemberDefinition $windowcode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru 
-	[void]$asyncwindow::ShowWindowAsync((Get-Process -PID $process_id).MainWindowHandle, 0) 
+  param (
+    [Parameter(Mandatory=$true)]
+    [UInt32]
+    $process_id
+  )
+  $windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);' 
+  $asyncwindow = Add-Type -MemberDefinition $windowcode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru 
+  [void]$asyncwindow::ShowWindowAsync((Get-Process -PID $process_id).MainWindowHandle, 0) 
 }
 
 
 function msgboxInfo
 {
-	param (
-		[Parameter(Mandatory=$true)]
-		[string]
-		$title,
-		[Parameter(Mandatory=$true)]
-		[string]
-		$message
-	)
-	[void][System.Windows.Forms.Messagebox]::Show($message, $title, [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $title,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $message
+  )
+  [void][System.Windows.Forms.Messagebox]::Show($message, $title, [System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 
